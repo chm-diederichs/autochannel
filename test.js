f202635a288656876091a50f29205bbc7cb48a2f
Christophe Diederichs <chm-diederichs@protonmail.com>
2022-11-10 22:58:58 +0000

    add chaos test

--- test.js
+++ test.js
@@ -85,6 +85,52 @@ async function main () {
   }
 }
 
+
+async function multichaos () {
+  const cores = [
+    new Hypercore(() => new RAM(), { valueEncoding: 'json' }),
+    new Hypercore(() => new RAM(), { valueEncoding: 'json' }),
+  ]
+
+  await Promise.all(cores.map(c => c.ready()))
+
+  cores.sort((a, b) => Buffer.compare(a.key, b.key))
+  const [al, bl] = cores
+
+  const ar = new Hypercore(() => new RAM(), al.key, { valueEncoding: 'json' })
+  const br = new Hypercore(() => new RAM(), bl.key, { valueEncoding: 'json' })
+
+  await ar.ready()
+  await br.ready()
+
+  const a = new Autochannel(al, br)
+  const b = new Autochannel(bl, ar)
+
+  replicate(al, ar)
+  replicate(bl, br)
+
+  let ai = 0
+  let bi = 0
+
+  const order = []
+
+  a.on('data', data => { order.push(data) })
+
+  while (a.local.length < 100) {
+    if (Math.random() < 0.7) await new Promise(r => setTimeout(r, 500))
+    for (let i = 0; i < Math.random() * 10; i++) await a.append(`a${ai++}`)
+    for (let i = 0; i < Math.random() * 10; i++) await b.append(`b${bi++}`)
+  }
+
+  let i = 0
+  for await (const data of b) {
+    if (data !== order[i++]) throw new Error('Bad order B' + i)
+    if (i === order.length) break
+  }
+
+  console.log('passed')
+}
+
 async function replicate (a, b) {
   const as = a.replicate(true)
   const bs = b.replicate(false)
